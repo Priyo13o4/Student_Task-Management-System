811b22659aa923a28f37306700e242a69634a5cb
Priyodip Mukhopadhyay <priyodip@Priyodips-MacBook-Pro.local>
2025-04-08 18:57:52 +0530

    Initial commit with models and admin setup

--- student/models.py
+++ student/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class student(models.Model) :
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=50,unique = True)
+    register_no = models.CharField(max_length=12,unique = True)
+    department = models.CharField(max_length=100)
+    attendance = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return self.name
+
--- task/models.py
+++ task/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from student.models import student
 
-# Create your models here.
+class task(models.Model) :
+
+    title = models.CharField(max_length= 200)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    priority = models.CharField(max_length=10,choices=[('Low','Low'),('Med','Medium'),('High','High')])
+    assigned_to_student = models.ForeignKey(student,on_delete= models.CASCADE)
+    completed = models.BooleanField(default = False)
+
+    def __str__(self):
+        return self.title
+
